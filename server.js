d8b65c4e217cb9667c004aac15b53626406fd690
Thomas Liu <tsmliu213@gmail.com>
2017-01-12 04:58:43 +0000

    public pages added

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-var express = include('express');
+var express = require('express');
+
+var app = express();
+app.use('/', express.static(__dirname + '/public'));
+
+
+app.listen(process.env.PORT || 8080);
