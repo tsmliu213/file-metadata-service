0fd59cc0a4fae9b235819204ef4469fabe20d3dd
Thomas Liu <tsmliu213@gmail.com>
2017-01-13 01:00:53 +0000

    no file selected case updated

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var upload = multer({ storage: storage });
 var app = express();
 app.use('/', express.static(__dirname + '/public'));
 app.post('/upload', upload.single('uploadfile'), function(req, res) {
-    res.send(JSON.stringify({"size": req.file.size}));
+    if(req.file == undefined) {
+        res.send(JSON.stringify({"error": "no file selected"}));
+    }
+    else {
+        res.send(JSON.stringify({"size": req.file.size}));
+    }
 });
-
 
 app.listen(process.env.PORT || 8080);
