e7d26e9e727ab63a341b325b117c951bfce99995
Thomas Liu <tsmliu213@gmail.com>
2017-01-13 00:56:46 +0000

    form and JSON response complete

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 var app = express();
 app.use('/', express.static(__dirname + '/public'));
+app.post('/upload', upload.single('uploadfile'), function(req, res) {
+    res.send(JSON.stringify({"size": req.file.size}));
+});
 
 
 app.listen(process.env.PORT || 8080);
